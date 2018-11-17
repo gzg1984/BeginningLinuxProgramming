f93698e3779f6f7ac0a3924dd68ddb1e52d9c11b
Zhigang Gao <gzg1984@troob.cn>
2018-11-16 21:09:20 -0500

    Chapter 11  -  POSIX Threads.

---
+++ chapter11/thread8.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+void *thread_function(void *arg);
+
+void *thread_function2(void *arg) 
+{
+    int i, res, j;
+    res = pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+    if (res != 0) {
+        perror("Thread pthread_setcancelstate failed");
+        exit(EXIT_FAILURE);
+    }
+    res = pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, NULL);
+    if (res != 0) {
+        perror("Thread pthread_setcanceltype failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("thread_function is running\n");
+    for(i = 0; i < 10; i++) {
+        printf("Thread is still running (%d)...\n", i);
+        sleep(1);
+    }
+    pthread_exit(0);
+}
+
+int main(int ac,char**av) 
+{
+    int res;
+    pthread_t a_thread;
+    void *thread_result;
+
+    res = pthread_create(&a_thread, NULL, thread_function, NULL);
+    if (res != 0) {
+        perror("Thread creation failed");
+        exit(EXIT_FAILURE);
+    }
+    sleep(3);
+    printf("Cancelling thread...\n");
+    res = pthread_cancel(a_thread);
+    if (res != 0) {
+        perror("Thread cancelation failed");
+        exit(EXIT_FAILURE);
+    }
+    printf("Waiting for thread to finish...\n");
+    res = pthread_join(a_thread, &thread_result);
+    if (res != 0) {
+        perror("Thread join failed");
+        exit(EXIT_FAILURE);
+    }
+    exit(EXIT_SUCCESS);
+}
+
+void *thread_function(void *arg) 
+{
+    pthread_t a_thread;
+    int i, res, j;
+    res = pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+    if (res != 0) {
+        perror("Thread3 pthread_setcancelstate failed");
+        exit(EXIT_FAILURE);
+    }
+    res = pthread_setcanceltype(PTHREAD_CANCEL_DEFERRED, NULL);
+    if (res != 0) {
+        perror("Thread3 pthread_setcanceltype failed");
+        exit(EXIT_FAILURE);
+    }
+    pthread_create(&a_thread, NULL, thread_function2, NULL);
+    printf("thread_function2 is running\n");
+    for(i = 0; i < 10; i++) {
+        printf("Thread2 is still running (%d)...\n", i);
+        sleep(1);
+    }
+    pthread_exit(0);
+}
